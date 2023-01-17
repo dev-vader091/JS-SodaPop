0b2b735f31b244a444fafdb322864d23cdc9f1e0
dev-vader091 <86165493+dev-vader091@users.noreply.github.com>
2023-01-17 17:52:47 -0600

    added JS functionality

--- js/site.js
+++ js/site.js
@@ -1,11 +1,77 @@
-function displayMessage() {
-  let msg = document.getElementById('message').value;
+// Controller function
+function getValues() {
+  // get user inputs from the page
+let firstNumber = document.getElementById('firstNumber').value;
+let secondNumber = document.getElementById('secondNumber').value;
 
-  Swal.fire(
-    {
-      backdrop: false,
-      title: 'App Name',
-      text: msg
-    }
-  );
+// parse inputs as numbers 
+firstNumber = parseInt(firstNumber);
+secondNumber = parseInt(secondNumber);
+
+// validate inputs are numbers 
+  if(Number.isInteger(firstNumber) && Number.isInteger(secondNumber)) {
+    // if they are, generate numbers
+    let numbersArray = generateSodaPop();
+    // then display
+    displaySodaPop(numbersArray, firstNumber, secondNumber);
+  }else {
+    // if not, alert requesting numbers only
+    Swal.fire(
+      {
+        icon: 'error',
+        title: 'Oops',
+        text: 'Only integers are allowed for SodaPop!'
+      }
+    );
+  }
+
+
 }
+
+// Logic function
+function generateSodaPop() {
+  // generate numbers 1-100
+  let numberList = [];
+
+
+  for(i = 1; i <= 100; i++) {
+    numberList.push(i);
+  }
+  
+  return numberList;
+  
+};
+
+// View function
+// display 
+function displaySodaPop(arrayOfNumbers,first, second) {
+  let tableBody = document.getElementById('results');
+
+  let tableHtml = "";
+  for(i = 0; i < arrayOfNumbers.length; i++) {
+    let value = arrayOfNumbers[i];
+    let divisibleByFirst = "soda";
+    let divisibleBySecond = "pop";
+    let divisibleByBoth = "sodapop";
+
+    let tableRow = "";
+
+    if (value % first == 0 && value % second == 0) {
+     tableRow = `<tr><td>${divisibleByBoth}</td></tr>`;
+    }else if(value % first == 0 && value % second !== 0) {
+      tableRow = `<tr><td>${divisibleByFirst}</td></tr>`;
+    }else if(value % first !== 0 && value % second == 0) {
+      tableRow = `<tr><td>${divisibleBySecond}</td></tr>`;
+    }
+    else {
+      tableRow = `<tr><td>${value}</td></tr>`;
+    }
+
+    tableHtml += tableRow;
+
+  }
+  
+  tableBody.innerHTML = tableHtml;
+  
+};
+  
